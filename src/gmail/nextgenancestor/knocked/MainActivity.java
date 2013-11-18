e4b72d902bf10a3aca879213ac3140c85419f2d9
roy09 <nextgen.ancestor@gmail.com>
2013-11-18 13:43:36 +0600

    ringtone switching works

--- src/gmail/nextgenancestor/knocked/MainActivity.java
+++ src/gmail/nextgenancestor/knocked/MainActivity.java
@@ -4,6 +4,7 @@ package gmail.nextgenancestor.knocked;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.media.AudioManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -22,35 +23,39 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		
-
 	}
 
 	public void turnOn(View view) {
-
 		status = !status;
-
+		
+		//Need to change status to another boolean to pass only through texts
+		AudioManager aManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+		if (status){
+			aManager.setRingerMode(aManager.RINGER_MODE_SILENT);
+			
+		} else {
+			aManager.setRingerMode(aManager.RINGER_MODE_NORMAL);
+			
+		}
 	}
 
 	
 	public void changePassword(View view) {
 		
-		
 		LayoutInflater li = LayoutInflater.from(this);
 		View promptsView = li.inflate(R.layout.prompt, null);
 
 		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-
 		alertDialogBuilder.setView(promptsView);
 
-		final EditText userInput = (EditText) promptsView
-				.findViewById(R.id.editTextDialogUserInput);
+		final EditText userInput = (EditText) promptsView.findViewById(R.id.editTextDialogUserInput);
 
 		alertDialogBuilder
 				.setCancelable(false)
 				.setPositiveButton("OK", new DialogInterface.OnClickListener() {
 					public void onClick(DialogInterface dialog, int id) {
 						password = userInput.getText().toString();
+						
 						EditText sth = (EditText) findViewById(R.id.cpwd);
 						sth.setText(password);
 					}
@@ -58,13 +63,13 @@ public class MainActivity extends Activity {
 				.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
 					public void onClick(DialogInterface dialog, int id) {
 						dialog.cancel();
+
 					}
 				});
 		
 		AlertDialog alertDialog = alertDialogBuilder.create();
-		
 		alertDialog.show();
-
+		
 	}
 
 	@Override
