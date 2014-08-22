c027e67609e056c730e7a7f23794784c92bd0c1b
Jyotirmoy Roy <nextgen.ancestor@gmail.com>
2014-08-23 03:29:18 +0600

    service working, now have to find a way to save status

--- src/gmail/nextgenancestor/knocked/SmsListener.java
+++ src/gmail/nextgenancestor/knocked/SmsListener.java
@@ -1,28 +1,26 @@
 package gmail.nextgenancestor.knocked;
 
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.media.AudioManager;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
 import android.util.Log;
-import android.view.View;
 
 public class SmsListener extends BroadcastReceiver {
 
-	private SharedPreferences preferences;
+
 	public String msgBody;
 	public String msg_from;
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		// TODO Auto-generated method stub
-
-		if (intent.getAction()
-				.equals("android.provider.Telephony.SMS_RECEIVED")) {
-			Bundle bundle = intent.getExtras(); // ---get the SMS message passed
+		if (intent.getAction().equals(
+				"android.provider.Telephony.SMS_RECEIVED")) {
+			Bundle bundle = intent.getExtras(); // ---get the SMS message
+												// passed
 												// in---
 			SmsMessage[] msgs = null;
 
@@ -32,7 +30,8 @@ public class SmsListener extends BroadcastReceiver {
 					Object[] pdus = (Object[]) bundle.get("pdus");
 					msgs = new SmsMessage[pdus.length];
 					for (int i = 0; i < msgs.length; i++) {
-						msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+						msgs[i] = SmsMessage
+								.createFromPdu((byte[]) pdus[i]);
 						msg_from = msgs[i].getOriginatingAddress();
 						msgBody = msgs[i].getMessageBody();
 					}
@@ -43,9 +42,11 @@ public class SmsListener extends BroadcastReceiver {
 		}
 
 		Log.d("msg_from", msg_from);
-		Log.d("password", gmail.nextgenancestor.knocked.MainActivity.password);
-		if (msgBody.equals(gmail.nextgenancestor.knocked.MainActivity.password)) {
-			if(gmail.nextgenancestor.knocked.MainActivity.status){
+		Log.d("password",
+				gmail.nextgenancestor.knocked.MainActivity.password);
+		if (msgBody
+				.equals(gmail.nextgenancestor.knocked.MainActivity.password)) {
+			if (gmail.nextgenancestor.knocked.MainActivity.status) {
 				changeSound(context);
 			}
 
@@ -71,4 +72,4 @@ public class SmsListener extends BroadcastReceiver {
 		}
 	}
 
-}
+} 
